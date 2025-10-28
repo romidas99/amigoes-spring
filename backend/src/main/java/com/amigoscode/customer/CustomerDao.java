21fa683ab466a375d9e143a0d190a13f9544211f
Kai Hsien Then <kai.hsien.then@ibm.com>
2025-10-28 16:51:48 +0800

    Implement methods in DAO which are spring repositories to make sql update and gets to the database

--- backend/src/main/java/com/amigoscode/customer/CustomerDao.java
+++ backend/src/main/java/com/amigoscode/customer/CustomerDao.java
@@ -12,4 +12,5 @@ public interface CustomerDao {
     void deleteCustomerById(Integer customerId);
     void updateCustomer(Customer update);
     Optional<Customer> selectUserByEmail(String email);
+    void updateCustomerProfileImageId(String profileImageId, Integer customerId);
 }
